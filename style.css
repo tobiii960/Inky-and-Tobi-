bec1af3e55ab73bbbe8890a4fd4c73a8f25a264b
tobiii960 <beatsaber16@gmail.com>
2026-07-01 01:03:43 -0400

    Create style.css

---
+++ style.css
@@ -0,0 +1,61 @@
+// =========================
+// MUSIC
+// =========================
+const music=document.getElementById("music");
+const button=document.getElementById("musicButton");
+
+button.onclick=function(){
+
+if(music.paused){
+
+music.play();
+
+button.innerHTML="🔇 Pause Music";
+
+}else{
+
+music.pause();
+
+button.innerHTML="🎻 Ambient Music";
+
+}
+
+}
+
+// =========================
+// LIGHTBOX
+// =========================
+
+const lightbox=document.getElementById("lightbox");
+
+const lightboxImage=document.getElementById("lightboxImage");
+
+const close=document.getElementById("close");
+
+document.querySelectorAll(".art img").forEach(image=>{
+
+image.onclick=function(){
+
+lightbox.style.display="flex";
+
+lightboxImage.src=this.src;
+
+}
+
+})
+
+close.onclick=function(){
+
+lightbox.style.display="none";
+
+}
+
+lightbox.onclick=function(e){
+
+if(e.target===lightbox){
+
+lightbox.style.display="none";
+
+}
+
+}
